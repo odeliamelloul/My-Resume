41188941e679bc18f5bbf0529772e6f528bfca17
odeliamelloul <odeliamel04@gmail.com>
2021-11-21 18:50:27 +0200

    first commit

--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -12,13 +12,14 @@ const HomePage=()=>
         <div className="home module mid">
             <div className="profile-header"> 
                 <h1> Odelia Melloul</h1>
-                <p className="p-4">A recent Computer Science graduate seeking my first
-                <br/>position as  junior software developer</p>
-
-                <div className="social-link">
+                <p >A recent Computer Science graduate seeking my first
+                <br/>position as  junior software developer
+             
+      
+                <div className="social-link p-2">
                      <a className="fa fa-github" href="https://github.com/odeliamelloul" ></a>
                      <a  className="fa fa-linkedin" href="https://linkedin.com/in/odelia-melloul" ></a>
-                </div>
+                </div></p>
             </div>
           </div>
           <About/>
